5e1317f3f4eaaf9ddbf265ef9a9004ae4eb6aee7
Aishwarya <amuthyala511@gmail.com>
2020-10-28 16:40:04 +0530

    welcome on master branch

---
+++ UserRegistrationExceptionHandling/src/main/java/com/blz/exception/userregistration/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.blz.exception.userregistration;
+
+public class UserRegistration
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to User Registration Exception Handling Problem");
+	}
+}
